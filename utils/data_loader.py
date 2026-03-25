e4efe05227699cf05c6d2035d068aa8e997041f8
Sakshi Dhandiya <shakshidhandiya02@gmail.com>
2026-03-25 19:25:59 +0530

    Add files via upload

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import streamlit as st
 
+@st.cache_data
+def load_data():
+    url = "https://drive.google.com/uc?id=129igTuB5wySC4zGVs3mD8xLDjA0sqhhS"
+    df = pd.read_csv(url, encoding='latin1')
+    return df
